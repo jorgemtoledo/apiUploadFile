ecb62bab69bb743caeddbe313ecf6135a7f8c72e
jorgemtoledo <jorge.toledo@gmail.com>
2020-03-07 13:15:02 -0300

    Add cors

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const morgan = require("morgan");
 const mongoose = require('mongoose');
 const path = require("path");
+const cors = require('cors');
 
 const app = express();
 
@@ -16,6 +17,7 @@ mongoose.connect(
     }
 );
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
@@ -23,4 +25,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')
 
 app.use(require("./routes"));
 
-app.listen(3334);
+app.listen(process.env.APP_PORT);
